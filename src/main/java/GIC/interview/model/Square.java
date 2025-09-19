a6a73c6fc00c7eeaafb7e8c9136be0cb051ae83f
XavierChen24 <41176459+XavierChen24@users.noreply.github.com>
2025-09-19 15:38:33 +0800

    Added square model. A minefield's field is a square. It contains revealed, ismine and the count of the surrounding mines.

--- src/main/java/GIC/interview/model/Square.java
+++ src/main/java/GIC/interview/model/Square.java
@@ -1,4 +1,37 @@
 package GIC.interview.model;
 
 public class Square {
+    private boolean revealed;
+    private boolean isMine;
+    private int adjacentMines;
+
+    public Square() {
+        this.revealed = false;
+        this.isMine = false;
+        this.adjacentMines = 0;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public boolean reveal() {
+        return this.revealed = true;
+    }
+
+    public void setIsMine() {
+        this.isMine = true;
+    }
+
+    public boolean getIsMine() {
+        return this.isMine;
+    }
+
+    public void setAdjacentMines(int input) {
+        this.adjacentMines = input;
+    }
+
+    public int getAdjacentMines() {
+        return this.adjacentMines;
+    }
 }
