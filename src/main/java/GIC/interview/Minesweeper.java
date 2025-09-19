7af35fcc78f08f57b111fafca1835f4b37558ca1
XavierChen24 <41176459+XavierChen24@users.noreply.github.com>
2025-09-19 11:36:50 +0800

    added code for main function

--- src/main/java/GIC/interview/Minesweeper.java
+++ src/main/java/GIC/interview/Minesweeper.java
@@ -1,7 +1,10 @@
 package GIC.interview;
 
+import GIC.interview.model.CommandProcessor;
+
 public class Minesweeper {
     public static void main(String[] args) {
-
+        CommandProcessor proc = new CommandProcessor();
+        proc.processInputs();
     }
 }
