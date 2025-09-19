23d852ecb1b5ce9e9bbc0a20518b29579508352d
XavierChen24 <41176459+XavierChen24@users.noreply.github.com>
2025-09-19 11:24:26 +0800

    Test case for validateInitialInput function should verify for 1. 35% of the grid can be mines 2. both values must be larger than 0. 3. minimum size of grid is 2

--- src/main/java/GIC/interview/model/CommandProcessor.java
+++ src/main/java/GIC/interview/model/CommandProcessor.java
@@ -11,14 +11,29 @@ public class CommandProcessor {
     }
 
     //This function takes in user inputs
-    public void processInputs(){
+    public void processInputs() {
+        int size = 0;
+        int minesCount = 0;
+        System.out.println("Welcome to Minesweeper!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Enter the size of the grid (e.g. 4 for a 4x4 grid): ");
+                size = Integer.parseInt(scanner.nextLine());
 
+                System.out.println("Enter the number of mines to place on the grid (maximum is 35% of the total squares): ");
+                minesCount = Integer.parseInt(scanner.nextLine());
+            }catch
+        }
     }
 
     //This function validates the bomb count and grid dimensions
-    public void validateInitialInput(){}
+    public void validateInitialInput(int size, int mines) {
+
+    }
 
     //This function validates the inputs for square selection
-    public void validateSquareSelection(){}
+    public void validateSquareSelection() {
+    }
 
 }
--- src/test/java/GIC/interview/model/CommandProcessorTest.java
+++ src/test/java/GIC/interview/model/CommandProcessorTest.java
@@ -1,29 +1,86 @@
 package GIC.interview.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 import java.io.PrintStream;
+import java.net.CookieManager;
 import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandProcessorTest {
 
+    final private CommandProcessor commandProcessor = new CommandProcessor();
+
     @Test
     void processInputs() {
         String simulatedInput = String.join("\n",
-            "4", //Size of the grid
-            "3",    //Number of mines
-            "D4",   //First selected square
-            "B1",   //Second selected square
-            "A1",   //Third selected square
-            "D1",   //Last selected square
-            " \n");
+                "4", //Size of the grid
+                "3",    //Number of mines
+                "D4",   //First selected square
+                "B1",   //Second selected square
+                "A1",   //Third selected square
+                "D1",   //Last selected square
+                " \n");
         Scanner mockScanner = new Scanner(simulatedInput);
-        PrintStream mockOut = mock(PrintStream.class);
-        
     }
+
+    @Test
+    public void validateInitialInputTests() {
+        assertAll("Validation scenarios",
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(0, 10);
+                    });
+                    assertEquals("Only positive non-zero values are accepted", exception.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(10, 0);
+                    });
+                    assertEquals("Only positive non-zero values are accepted", exception.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(-1, 10);
+                    });
+                    assertEquals("Only positive non-zero values are accepted", exception.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(10, -1);
+                    });
+                    assertEquals("Only positive non-zero values are accepted", exception.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(10, -1);
+                    });
+                    assertEquals("Only positive non-zero values are accepted", exception.getMessage());
+                },
+                () -> {
+                    Assertions.assertDoesNotThrow(() -> {
+                        commandProcessor.validateInitialInput(10, 35);
+                    }, "When the mines is 35% of the grids, it should not throw an error");
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(10, 36);
+                    });
+                    assertEquals("Maximum is 35% of the total squares", exception.getMessage());
+                },
+                () -> {
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                        commandProcessor.validateInitialInput(3, 36);
+                    });
+                    assertEquals("Minimum size is 3", exception.getMessage());
+                }
+        );
+    }
+
 }
